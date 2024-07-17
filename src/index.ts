500623f4442d24f0111ee310a694605ed8840da0
luchodf <luchodellafelice@hotmail.com>
2024-07-17 11:12:19 +0200

    Configure express server and authenticate database conection

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { sequelize } from "./database/database";
+
+async function main() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection etabli avec success");
+
+    const app = express();
+
+    app.listen(3000, () => {
+      console.log("Server on port", 3000);
+    });
+  } catch (error) {
+    console.error("Impossible de se connecter à la base de donnés",error);
+  }
+}
+
+main();
