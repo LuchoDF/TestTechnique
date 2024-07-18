7e26fcc3a5ceda02ae4626ebf94f7feb528aef7d
luchodf <luchodellafelice@hotmail.com>
2024-07-19 00:55:44 +0200

    Add tag methods

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getVideos, createVideo, updateVideo, deleteVideo } from "../controllers/videos.controller";
+import { createTag, deleteTag } from "../controllers/tags.controller";
 
 export const routes = Router();
 
@@ -8,13 +9,10 @@ routes.get("/videos", getVideos);
 routes.post("/videos", createVideo);
 routes.put("/videos/:id",updateVideo);
 routes.delete("/videos/:id",deleteVideo);
-routes.get("/videos/:id");
 
-routes.get("/tags");
-routes.post("/tags");
-routes.put("/tags/:id");
-routes.delete("/tags/:id");
-routes.get("/tags/:id");
+routes.post("/tags", createTag);
+routes.delete("/tags/:id",deleteTag);
+
 
 routes.post("/videos/:id/tags")
 routes.delete("/videos/:id/tags/:id")
