23d9dd8984089818695ae38b3e3048032c0f587e
luchodf <luchodellafelice@hotmail.com>
2024-07-18 12:03:04 +0200

    Import routes and json parser

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import { sequelize } from "./database/database";
+import { routes } from "./routes/routes";
 
 async function main() {
   try {
-    await sequelize.sync();
+    await sequelize.sync({force: false});
     await sequelize.authenticate();
     console.log("Connection etabli avec success");
 
+    
     const app = express();
-
+    // Middleware para parsear JSON
+    app.use(express.json());
+    app.use(routes);
     app.listen(3000, () => {
       console.log("Server on port", 3000);
     });
