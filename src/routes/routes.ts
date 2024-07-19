3811bdd4d6b915141d6f8b6695eb59e007a2a605
luchodf <luchodellafelice@hotmail.com>
2024-07-19 11:56:08 +0200

    Comment

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,15 +5,16 @@ import { addTagToVideo, removeTagFromVideo } from "../controllers/videosTags.con
 
 export const routes = Router();
 
-
+/** Video CRUD routes **/
 routes.get("/videos", getVideos);
 routes.post("/videos", createVideo);
 routes.put("/videos/:id",updateVideo);
 routes.delete("/videos/:id",deleteVideo);
 
+/** Tag routes **/
 routes.post("/tags", createTag);
 routes.delete("/tags/:id",deleteTag);
 
-
+/** Tag to and from videos routes **/
 routes.post("/videostags/:id",addTagToVideo);
 routes.delete("/videostags/:id/tags/:id_tag",removeTagFromVideo)
