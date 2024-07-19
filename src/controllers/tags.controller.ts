3d32a746407de824e36b0c53db696ad2aa7506ae
luchodf <luchodellafelice@hotmail.com>
2024-07-19 13:22:40 +0200

    Add comments and tsDoc

--- src/controllers/tags.controller.ts
+++ src/controllers/tags.controller.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from "express";
 import { Tag } from "../models/tags";
 
+/**
+ *Création d'une tag
+ *
+ * @param Request req - L'objet de la réquete
+ * @param Response res - L'objet de la réponse
+ * @returns Promise<void>  
+ */
 export const createTag = async (req: Request, res: Response) => {
-  const { valeur } = req.body;
   try {
+    //On obtient les donnés à partir de la requête
+    const { valeur } = req.body;
+    //On crée une instance tag à partir du donnés recuperés et on la sauvegarde
     const newTag = await Tag.create({
       valeur,
     });
@@ -13,9 +22,19 @@ export const createTag = async (req: Request, res: Response) => {
   }
 };
 
+
+/**
+ *Supprimer une tag
+ *
+ * @param Request req - L'objet de la réquete
+ * @param Response res - L'objet de la réponse
+ * @returns Promise<void>  
+ */
 export const deleteTag = async (req: Request, res: Response) => {
   try {
+    //On obtient l'id à partir de l'url
     const { id } = req.params;
+    //On supprime le tag qui correspond à l'id obtenu
     await Tag.destroy({
       where: {
         id_tag: id,
